09eeb2e80a0c867bc50af3b2c6883aa3ccb1aa10
fargabuanik <fargabanik@gmail.com>
2022-08-03 21:53:26 -0400

    updata

--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -1,13 +1,5 @@
 package hw5Q3Constructor;
 
-import java.awt.TexturePaint;
-
-/*Create object as many as you want under the main method. Initialize constructors. I want the first outcome in the console as --> This is from default Constructor of Student class. in the next line, The second outcome -- > Student Name: <your name>, ID: <your id>, Sex: <your sex in char>, Grade: <your grade> and Java Programmer? Ans: true. Organize the code. Push in the github. Paste your github link below.
-50 points*/
-
-
-
-
 public class StudentTest {
 
 	public static void main(String[] args) {
