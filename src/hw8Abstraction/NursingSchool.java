7b29c50b04f3c662064e1ac2fb3157e2644231a2
fargabuanik <fargabanik@gmail.com>
2022-08-23 18:57:26 -0400

    updata

--- src/hw8Abstraction/NursingSchool.java
+++ src/hw8Abstraction/NursingSchool.java
@@ -3,6 +3,6 @@ package hw8Abstraction;
 public abstract class NursingSchool {
 	public abstract void hygiene();
 	public void caring() {
-		
+		System.out.println("This non-abstract method from NursingSchool Class");
 	}
 }
--- src/hw8Abstraction/Univetsity.java
+++ src/hw8Abstraction/Univetsity.java
@@ -1,13 +1,13 @@
 package hw8Abstraction;
 
-/* An Interface can't inherit a regular class or an abstract class by extends keyword. An Interface can inherit more than one Interface by extends keyword. implements keyword can't be used for inheritance inside Interface
- */
-public interface Univetsity extends College, Hospital{
+//Interfaces cannot have constructors,
+//because Interface can contain only abstract method and non-abstract method(default,static)
+public interface Univetsity{
 	public void classSize();
 	public void playGround();
 	public void teacher();
+	
+	//public University() { 
+	//}
+	}
 
-
-
-
-}
