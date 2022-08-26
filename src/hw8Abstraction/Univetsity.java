67b444ae2ffabbb02760c36c9521ac7df9fc4458
fargabuanik <fargabanik@gmail.com>
2022-08-26 16:11:21 -0400

    updata

--- src/hw8Abstraction/Univetsity.java
+++ src/hw8Abstraction/Univetsity.java
@@ -1,7 +1,7 @@
 package hw8Abstraction;
 
 /*Interfaces cannot have constructors,
-/because Interface can contain only abstract method and non-abstract method(default,static*/
+/because Interface can contain only abstract method and non-abstract method (default,static)*/
 public interface Univetsity{
 	public void classSize();
 	public void playGround();
